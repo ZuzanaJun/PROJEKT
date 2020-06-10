33f223b7b3afd3544d39bcec4b2aea0b4dd5f96a
ZuzanaJun <64091379+ZuzanaJun@users.noreply.github.com>
2020-06-10 23:27:07 +0200

    Vytvoření tlačítek v Home.vue
    přidány vstupní odkazovací tlačítka pomocí vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="homepage">
-
+        <v-app>
+            
+            <div class="text-center">
+                <v-btn class="ma-2" outlined color="indigo"><router-link to="/workspace">Začít</router-link></v-btn>     
+                <v-btn class="ma-2" outlined color="indigo"><router-link to="/gallery">Přejít do galerie</router-link></v-btn>     
+            </div>
+        
+        </v-app>
     </div>
 </template>
 
