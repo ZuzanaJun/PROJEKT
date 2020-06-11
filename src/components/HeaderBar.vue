7656bb5814e02441f92fffd2ce07674ce4b1ad06
ZuzanaJun <64091379+ZuzanaJun@users.noreply.github.com>
2020-06-11 12:48:37 +0200

    Header přesunut do samostatné komponenty
    Vytvořena komponenta HeaderBar.vue

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+            <v-app-bar dense dark >  
+                <v-toolbar-title>Page title</v-toolbar-title>
+        
+                <v-spacer></v-spacer>
+        
+                <v-menu left bottom >
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                        icon
+                        v-bind="attrs"
+                        v-on="on"
+                        >
+                        <v-icon>mdi-dots-vertical</v-icon>
+                        </v-btn>
+                    </template>
+        
+                    <v-list>
+                        <v-list-item
+                        v-for="n in 5"
+                        :key="n"
+                        @click="() => {}"
+                        >
+                        <v-list-item-title>Option {{ n }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </v-app-bar>
+        </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
