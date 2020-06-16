37487c50c72f8edec421f1f0855d7828f7b0a7d3
veera-z <veronika.zimcikova@gmail.com>
2020-06-16 17:15:20 +0200

    pridala jsem pozadi

---
+++ public/data/dataBackground.js
@@ -0,0 +1,15 @@
+export default [
+    {id: 1001, name: 'b001.png', type: 'pattern', url: '/images/background/b001.png'},
+    {id: 1002, name: 'b002.png', type: 'pattern', url: '/images/background/b002.png'},
+    {id: 1003, name: 'b003.png', type: 'pattern', url: '/images/background/b003.png'},
+    {id: 1004, name: 'b004.png', type: 'foto', url: '/images/background/b004.png'},
+    {id: 1005, name: 'b005.png', type: 'foto', url: '/images/background/b005.png'},
+    {id: 1006, name: 'b006.png', type: 'foto', url: '/images/background/b006.png'},
+    {id: 1007, name: 'b007.png', type: 'foto', url: '/images/background/b007.png'},
+    {id: 1008, name: 'b008.png', type: 'foto', url: '/images/background/b008.png'},
+    {id: 1009, name: 'b009.png', type: 'foto', url: '/images/background/b009.png'},
+    {id: 1010, name: 'b010.png', type: 'foto', url: '/images/background/b010.png'},
+    {id: 1011, name: 'b011.png', type: 'foto', url: '/images/background/b011.png'},
+    
+
+]
