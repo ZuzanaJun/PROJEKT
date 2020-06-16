0cddf9c7edb8faf5ba74050d2caebf4a1c986739
veera-z <veronika.zimcikova@gmail.com>
2020-06-16 21:34:15 +0200

    rozdelany bar

---
+++ src/components/SidebarRight.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="sidebarright" id="sidebarright">
+      <!-- <p> zkousim this shit </p> -->
+        <div class="cutouts">
+            <div 
+                class="cutouts__item"
+                v-for="cut in dataCutouts"
+                v-bind:key="cut.id"
+            >
+
+            </div>
+        </div>
+
+
+      <div class="colors-picker">
+          <div 
+            class="colors-picker__color"
+            v-for="color in colors.reverse()"
+            v-bind:key="color.name"
+            v-bind:style="{background: color.display, height: `${100 / colors.length}%`}"
+            v-on:click="handleClick(color.name)"
+          >
+
+          </div>
+
+
+      </div>
+    </div>
+</template>
+
+<script>
+import dataCutouts from '../dataCutouts'
+
+export default {
+    data() {
+        return {
+            colors: [
+                {name: 'white', display: '#fff'},
+                {name: 'yellow', display: '#FFD200'},
+                {name: 'blue', display: '#38B7D0'},
+                {name: 'pink', display: '#FD68D0'},
+                {name: 'red', display: '#DA2900'},
+                {name: 'beige', display: '#FDEEC0'},
+                {name: 'black', display: '#000'},
+            ],
+            dataCutouts
+        }
+    },
+    methods: {
+        handleClick(name) {
+            alert(name);
+
+        }
+    }
+}
+</script>
+
+<style>
+
+.sidebarright {
+    position: absolute;
+    right: 0;
+    top: 48px;
+    bottom: 0;
+    display: flex;
+}
+
+.colors-picker {
+    height: 100%;
+    width: 35px;
+    background-color: antiquewhite;
+}
+
+.cutouts {
+    height: 100%;
+    width: 300px;
+    border: black 2px solid;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.cutouts__item {
+    width: 140px;
+    height: 150px;
+    background-color: blueviolet;
+}
+
+</style>
--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -4,6 +4,8 @@
 
         <sidebarleft />
 
+        <sidebarright />
+
 
     </div>
 </template>
@@ -11,11 +13,13 @@
 <script>
 import HeaderBar from '../components/HeaderBar.vue';
 import SidebarLeft from '../components/SidebarLeft.vue';
+import SidebarRight from '../components/SidebarRight.vue';
 
 export default {
     components: {
         headerbar: HeaderBar,
         sidebarleft: SidebarLeft,
+        sidebarright: SidebarRight,
     }
     
 }
