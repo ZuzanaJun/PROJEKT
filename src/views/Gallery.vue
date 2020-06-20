5272303755ec4116eba6406e4688f77835080ca4
veera-z <veronika.zimcikova@gmail.com>
2020-06-20 09:29:59 +0200

    trocha galerie

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -11,30 +11,20 @@
            
         </div>
 
-        <div class="gallery">
-            <div 
-                class="gallery__item"
-                v-for="prefin in prefins"
-                v-bind:key="prefin.id"
-                >
-                <img v-bind:src="prefin.url" alt="collages" class="gallery__img">
+        <galleryitem />
 
-            </div>
-        </div>
+        
     </div>
 </template>
 
 
 <script>
+import GalleryItem from '../components/GalleryItem'
+
 export default {
-    data() {
-        return {
-            prefins: [
-                {id: 5001, name: 'prefinished1.jpg', type: 'pattern', url: '/images/prefinished/prefinished1.jpg'},
-                {id: 5002, name: 'prefinished2.jpg', type: 'pattern', url: '/images/prefinished/prefinished2.jpg'},
-                {id: 5003, name: 'prefinished3.jpg', type: 'pattern', url: '/images/prefinished/prefinished3.jpg'}
-            ]
-        }
+    
+    components: {
+        galleryitem: GalleryItem
     }
 }
 </script>
@@ -51,10 +41,5 @@ export default {
     
     
 }
-
-.gallery {
-    display: flex;
-}
-
 
 </style>
