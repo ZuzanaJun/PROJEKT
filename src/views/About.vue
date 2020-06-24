e2c295ba2e9d8bcff4b1ff2a3e08c03aa2fc299a
ZuzanaJun <64091379+ZuzanaJun@users.noreply.github.com>
2020-06-24 14:17:01 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,75 +19,62 @@
     <div class="about__content">
       <h1>Jak tento projekt vznikal</h1>
       <div class="fist_img">
-          <div class="about_foto">
-            <img src="/images/fotos/hackathon.jpg" alt="foto" />
-          </div>
+        <div class="about_foto">
+          <img src="/images/fotos/hackathon.jpg" alt="foto" />
+        </div>
 
-          <!-- <div class="about_foto">
+        <!-- <div class="about_foto">
             <img src="/images/fotos/mym.jpg" alt="foto" />
-          </div> -->
+        </div>-->
       </div>
 
-      <p>Projekt Kolážovník vznikal v rámci Digitální Akademie Web pod hlavičkou Czechitas v Brně. Dohromady se daly Véra se Zuzkou, které zjistily, že mají podobnou potřebu kreativního vyjádření. S&nbsp;impulzem pro tento projekt přišla Veronika, ale nebyla to první věc co ji napadla. Hledala nějaký nápad s větším sociálním nebo edukativním dopadem, ovšem pocit, že všechno už bylo vymyšleno a&nbsp;zpracováno, ji vrátilo právě k myšlence udělat si projekt pro radost. A právě to, co ji baví, nabídnout i&nbsp;ostatním k&nbsp;vyzkoušení. Zuzku myšlenka nadchla, jelikož sama čas od času vyplní volný čas tím, že vezme do ruky staré časopisy, nůžky, lepidlo a vytvoří abstraktní kompozici. Holky daly hlavy dohromady a představa o Kolážovníku se začala formovat do realných obrysů.</p>
+      <p>Projekt Kolážovník vznikal v rámci Digitální Akademie Web pod hlavičkou Czechitas v Brně. Dohromady se daly Véra se Zuzkou, které zjistily, že mají podobnou potřebu kreativního vyjádření. S&nbsp;impulzem pro tento projekt přišla Veronika, ale nebyla to první věc co ji napadla. Hledala nějaký nápad s větším sociálním nebo edukativním dopadem, ovšem pocit, že všechno už bylo vymyšleno a&nbsp;zpracováno, ji vrátilo právě k myšlence udělat si projekt pro radost. A&nbsp;právě to, co ji baví, nabídnout i&nbsp;ostatním k&nbsp;vyzkoušení. Zuzku myšlenka nadchla, jelikož sama čas od času vyplní volný čas tím, že vezme do ruky staré časopisy, nůžky, lepidlo a vytvoří abstraktní kompozici. Holky daly hlavy dohromady a&nbsp;představa o Kolážovníku se začala formovat do realných obrysů.</p>
       <div class="about_foto">
         <img src="/images/fotos/meetmentor.jpg" alt="foto" />
       </div>
-       
 
-
-
-
-        <div class="container__us">
-          <img class="container__image-v" src="/images/fotos/ve.jpg" alt="foto" >
-          <div class="container__text-v">
-            <p class="name">Veronika Zimčíková</p> 
-            <p>Véra se zajímá o umění, grafický design a architekturu.</p>
-          </div>
+      <div class="container__us">
+        <img class="container__image-v" src="/images/fotos/ve.jpg" alt="foto" />
+        <div class="container__text-v">
+          <p class="name">Veronika Zimčíková</p>
+          <p>Véra se zajímá o umění, grafický design a architekturu.</p>
         </div>
-
-        <div class="container__us">
-          <img class="container__image-v" src="/images/fotos/zu.jpg" alt="foto" >
-          <div class="container__text-z">
-            <p class="name">Zuzana Junkerová</p> 
-            <p>Zuzka má ráda všechno živé a přírodu i neživou, zejména podzemní svět jeskyní.</p>
-          </div>
-        </div>
-<div class="mentors">
-  <div>
-      <p>Dalším velmi důležitým elementem byl mentor Martin Čapek, který se nezalekl složitého projektu a&nbsp;šel do toho s námi.</p>
-      <div class="about_foto-profil">
-          <img src="/images/fotos/ma.png" alt="foto" />
-        </div>
-  </div>
-  <div>
-      <p>Kolážovník by pravděpodobně nevzniknul bez skvělého Luďka Rolečka, který nám byl lektorem i&nbsp;oporou a jeho snaha nám předávat informace byla nekonečná, děkujeme za cenné rady a hlavně trpělivost. </p>
-      <div class="about_foto-profil">
-          <img src="/images/fotos/ludek.jpeg" alt="foto" />
-        </div>
-  </div>
-</div>
- 
-
-
-
       </div>
 
-        
-    
+      <div class="container__us">
+        <img class="container__image-v" src="/images/fotos/zu.jpg" alt="foto" />
+        <div class="container__text-z">
+          <p class="name">Zuzana Junkerová</p>
+          <p>Zuzka má ráda všechno živé a přírodu i neživou, zejména podzemní svět jeskyní.</p>
+        </div>
+      </div>
+      <div class="mentors">
+        <div class="mentor">
+          <p>Dalším velmi důležitým elementem byl mentor Martin Čapek, který se nezalekl složitého projektu a&nbsp;šel do toho s námi.</p>
+          <div class="about_foto-profil">
+            <img src="/images/fotos/ma.png" alt="foto" />
+          </div>
+        </div>
+        <div class="mentor">
+          <p>Kolážovník by pravděpodobně nevzniknul bez skvělého Luďka Rolečka, který nám byl lektorem i&nbsp;oporou a jeho snaha nám předávat informace byla nekonečná, děkujeme za cenné rady a hlavně trpělivost.</p>
+          <div class="about_foto-profil">
+            <img src="/images/fotos/ludek.jpeg" alt="foto" />
+          </div>
+        </div>
+      </div>
+    </div>
 
-  <footerbar />
+    <footerbar />
   </div>
 </template>
 
 <script>
-import FooterBar from '../components/FooterBar.vue';
+import FooterBar from "../components/FooterBar.vue";
 
 export default {
   components: {
-        
-        footerbar: FooterBar,
-
-    }
+    footerbar: FooterBar
+  }
 };
 </script>
 
@@ -100,68 +87,55 @@ export default {
   font-family: "Bellota", cursive;
   color: white;
   font-size: 17px;
-  
 }
-
 
 .name {
-    font-size: 20px;
-    font-weight: bold;
+  font-size: 20px;
+  font-weight: bold;
 }
 
-.about_foto img{
-    width: calc((70% - 20px) / 1);
-    margin: 10px;
-    padding: 10px;
+.about_foto img {
+  width: calc((70% - 20px) / 1);
+  margin: 10px;
+  padding: 10px;
 }
-
 
 .about__content {
-/*     flex-shrink: 1;
+  /*     flex-shrink: 1;
     margin: 10px; */
-     margin: 25px auto 60px auto;
+  margin: 25px auto 60px auto;
   max-width: 50em;
- text-align: center;
- border: 1px solid white;
- padding: 20px 0;
-
-   
+  text-align: center;
+  border: 1px solid white;
+  padding: 20px 0;
 }
 
 .about__content h1 {
-        text-align: center;
-          padding: 10px;
-
-
+  text-align: center;
+  padding: 10px;
 }
 
 .about__content p {
-    margin: 20px;
-    padding: 15px;
-    
-};
-
+  margin: 20px;
+  padding: 15px;
+  text-align: justify;
+}
 
 .about_foto-profil {
   display: flex;
-  
 }
 
-
-
-
 .container__us {
-    display: flex;
-    margin: 30px;
-    padding: 40px;
-   
+  display: flex;
+  margin: 30px;
+  padding: 40px;
 }
 
 .container__image-z {
   margin: 0 50px;
   width: 250px;
   object-fit: contain;
-/*   justify-self: flex-start;
+  /*   justify-self: flex-start;
  */
   order: 1;
 }
@@ -169,37 +143,38 @@ export default {
   margin: 0 50px;
   width: 250px;
   object-fit: contain;
-/*   justify-self: flex-end;
+  /*   justify-self: flex-end;
  */
-   order: 2;
-
+  order: 2;
 }
 
 .container__text-z {
   flex: 1 1 auto;
   text-align: left;
-    order: 2;
-
+  order: 2;
 }
 .container__text-v {
   flex: 1 1 auto;
   text-align: left;
-    order: 1;
-
+  order: 1;
 }
 
-.mentors{
+.mentors {
   display: flex;
   height: 150px;
   margin: 30px;
   text-align: center;
-
 }
-.mentors img{
+.mentors img {
   height: 250px;
+  margin: 10px;
 }
-
+.mentor {
+  flex-basis: 50%;
+}
 .mentors p {
-  height: 100px;
+  text-align: justify;
+  padding: 0;
+  height: 120px;
 }
 </style>
